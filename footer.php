9574e4f9264597edee87ab7458ed7e00ac1c4173
jawittdesigns <Killjoy11>
2016-02-02 00:10:04 -0500

    CSS

--- footer.php
+++ footer.php
@@ -1,30 +1,33 @@
 <footer class="body__footer" role="contentinfo">
   <div class="content__wrapper">
-    <?php get_template_part( 'template-parts/menu', 'footer' ); ?>
+    <?php get_template_part('template-parts/menu', 'footer');?>
+    <div class="finder">
+      <iframe src="http://ohiochildcarefinder.org/find-child-care-connect/8.html" frameborder="0" scrolling="no" target="_blank"></iframe>
+    </div>
     <div class="footer__info">
       <ul>
         <li>
           <a href="<?php echo home_url(); ?>/privacy-policy/">
-            <?php _e( 'Privacy Policy', FCWP_TEXTDOMAIN ); ?>
+            <?php _e('Privacy Policy', FCWP_TEXTDOMAIN);?>
           </a>
         </li>
         <li>
           <a href="<?php echo home_url(); ?>/contact/">
-            <?php _e( 'Contact', FCWP_TEXTDOMAIN ); ?>
+            <?php _e('Contact', FCWP_TEXTDOMAIN);?>
           </a>
         </li>
         <li>
           <a href="<?php echo home_url(); ?>/staff-email/">
-            <?php _e( 'Staff Email', FCWP_TEXTDOMAIN ); ?>
+            <?php _e('Staff Email', FCWP_TEXTDOMAIN);?>
           </a>
         </li>
         <li>
-          <?php _e( '&copy; ' . date('Y') . ' Action for Children. All Rights Reserved.' , FCWP_TEXTDOMAIN ); ?>
+          <?php _e('&copy; ' . date('Y') . ' Action for Children. All Rights Reserved.', FCWP_TEXTDOMAIN);?>
         </li>
       </ul>
     </div>
   </div>
 </footer>
-<?php wp_footer(); ?>
+<?php wp_footer();?>
 </body>
 </html>
